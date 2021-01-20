548ecf43e19691c22d1b4ab5a71c94146440e24c
mcapurri <m.capurri@gmail.com>
2021-01-20 20:09:08 +0100

    work in progres

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,28 @@
 class MemoryGame {
-  constructor(cards){
-    this.cards = cards;
-    // add the rest of the class properties here
-  }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+    constructor(cards) {
+        this.cards = cards;
+        this.pickedCards = [];
+        this.pairsClicked = 0;
+        this.pairsGuessed = 0;
+    }
+    shuffleCards(cards) {
+        // let i = cards.length, k , temp;      // k is to generate random index and temp is to swap the values
+        // while(--i > 0){
+        // k = Math.floor(Math.random() * (i+1));
+        // temp = cards[k];
+        // cards[k] = cards[i];
+        // cards[i] = temp;
+        // let shuffled = Math.floor(Math.random() * cards.length);
+        // console.log('shuffled', shuffled);
+        // return cards;
+    }
+    checkIfPair(card1, card2) {
+        this.pairsClicked++;
+        if (card1 === card2) {
+            this.pairsGuessed++;
+            return true;
+        }
+        return false;
+    }
+    isFinished() {}
 }
